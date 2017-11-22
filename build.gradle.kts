c28e1abbbb20db54306c11444589b2c4e5c314c1
Richard Wardle <richard.wardle@thomsonreuters.com>
2017-11-22 11:47:52 +0000

    Upgrade Kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     application
     java
     idea
-    id("org.jetbrains.kotlin.jvm") version "1.1.51"
+    id("org.jetbrains.kotlin.jvm") version "1.1.60"
     id("org.unbroken-dome.test-sets") version "1.4.2"
 }
 
@@ -47,22 +47,26 @@ idea {
 }
 
 dependencies {
+    val kotlinVersion = "1.1.60"
+    val serenityVersion = "1.5.3"
+    val smackVersion = "4.2.0-beta2"
+
     compileOnly("com.google.auto.value:auto-value:1.2")
-    compile("org.igniterealtime.smack:smack-im:4.2.0-beta2")
-    compile("org.igniterealtime.smack:smack-tcp:4.2.0-beta2")
-    runtime("org.igniterealtime.smack:smack-core:4.2.0-beta2")
-    runtime("org.igniterealtime.smack:smack-java7:4.2.0-beta2")
+    compile("org.igniterealtime.smack:smack-im:$smackVersion")
+    compile("org.igniterealtime.smack:smack-tcp:$smackVersion")
+    runtime("org.igniterealtime.smack:smack-core:$smackVersion")
+    runtime("org.igniterealtime.smack:smack-java7:$smackVersion")
     testCompile("junit:junit:4.12")
     testCompile("org.mockito:mockito-core:2.+")
     testCompile("org.hamcrest:hamcrest-library:1.3")
     testCompile("com.natpryce:make-it-easy:4.0.1")
-    add("endToEndTestCompile", kotlin("stdlib-jre8", "1.1.51"))
-    add("endToEndTestCompile", kotlin("test", "1.1.51"))
-    add("endToEndTestCompile", kotlin("test-junit", "1.1.51"))
+    add("endToEndTestCompile", kotlin("stdlib-jre8", kotlinVersion))
+    add("endToEndTestCompile", kotlin("test", kotlinVersion))
+    add("endToEndTestCompile", kotlin("test-junit", kotlinVersion))
     add("endToEndTestCompile", "org.assertj:assertj-swing-junit:3.4.0")
-    add("endToEndTestCompile", "net.serenity-bdd:serenity-core:1.5.3")
-    add("endToEndTestCompile", "net.serenity-bdd:serenity-screenplay:1.5.3")
-    add("endToEndTestCompile", "net.serenity-bdd:serenity-junit:1.5.3")
+    add("endToEndTestCompile", "net.serenity-bdd:serenity-core:$serenityVersion")
+    add("endToEndTestCompile", "net.serenity-bdd:serenity-screenplay:$serenityVersion")
+    add("endToEndTestCompile", "net.serenity-bdd:serenity-junit:$serenityVersion")
     add("endToEndTestCompile", java.sourceSets["test"].output.classesDirs)
     add("integrationTestCompile", java.sourceSets["endToEndTest"].output.classesDirs)
 }
